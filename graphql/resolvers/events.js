12a199a1396b4b0044fae8723ced2a7194439006
Nemanja Stojanovic <nemanja.stojanovic@mqsoft.rs>
2019-08-14 16:11:00 +0200

    Refactore the whole project

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,38 @@
+const Event = require("../../models/event");
+const { dateToString } = require("../../helpers/date");
+const { transformEvent } = require("./merge");
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => transformEvent(event));
+    } catch (error) {
+      throw error;
+    }
+  },
+  createEvent: async args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: dateToString(args.eventInput.date),
+      creator: "5d505f68d0394c2050001b41"
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById("5d505f68d0394c2050001b41");
+      if (!creator) {
+        throw new Error("User not found.");
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+      return createdEvent;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+};
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,45 +1,56 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
-type Event {
-  _id: ID!
-  title: String!
-  description: String!
-  price: Float!
-  date: String!
-  creator: User!
-}
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
 
-type User {
-  _id: ID!
-  email: String!
-  password: String
-  createdEvents: [Event!]
-}
+  input UserInput {
+    email: String!
+    password: String!
+  }
 
-input EventInput {
-  title: String!
-  description: String!
-  price: Float!
-  date: String!
-}
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+    creator: User!
+  }
 
-input UserInput {
-  email: String!
-  password: String!
-}
+  type User {
+    _id: ID!
+    email: String!
+    password: String
+    createdEvents: [Event!]
+  }
 
-type RootQuery {
-  events: [Event!]!
-}
+  type Booking {
+    _id: ID!
+    user: User!
+    event: Event!
+    createdAt: String!
+    updatedAt: String!
+  }
 
-type RootMutation {
-  createEvent(eventInput: EventInput): Event
-  createUser(userInput: UserInput): User
-}
+  type RootQuery {
+    events: [Event!]!
+    bookings: [Booking!]!
+  }
 
-schema {
-  query: RootQuery
-  mutation: RootMutation
-}
+  type RootMutation {
+    createEvent(eventInput: EventInput): Event
+    createUser(userInput: UserInput): User
+    bookEvent(eventId: ID!): Booking!
+    cancelBooking(bookingId: ID!): Event!
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
 `);
